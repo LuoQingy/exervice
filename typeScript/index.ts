437ea7ec1b1912b0848f7a6ed1cbf99a217fcf38
LuoQingy <1075899733@qq.com>
2019-04-13 14:07:18 +0800

    ts

--- typeScript/index.ts
+++ typeScript/index.ts
@@ -2,3 +2,32 @@
     var message:string = "Hello World" 
 console.log(message)
 }
+{
+    var message:string = "Hello World" 
+console.log(message)
+}
+{
+    var message:string = "Hello World" 
+console.log(message)
+}
+
+{
+    var message:string = "Hello World" 
+console.log(message)
+}
+{
+    var message:string = "Hello World" 
+console.log(message)
+}
+{
+    var message:string = "Hello World" 
+console.log(message)
+}
+{
+    var message:string = "Hello World" 
+console.log(message)
+}
+{
+    var message:string = "Hello World" 
+console.log(message)
+}
