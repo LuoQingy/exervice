576bdd94f766c8edfa03649d5f9a3e2832a6680e
LuoQingy <1075899733@qq.com>
2019-04-25 09:05:43 +0800

    44228585

--- typeScript/index.js
+++ typeScript/index.js
@@ -26,6 +26,14 @@
     var message = "Hello World";
     console.log(message);
 }
+{
+    var message = "Hello World";
+    console.log(message);
+}
+{
+    var message = "Hello World";
+    console.log(message);
+}
 ;
 {
     var message = "Hello World";
