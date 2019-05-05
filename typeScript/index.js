47efc36af7cf5727f682738942ec457445915d11
LuoQingy <1075899733@qq.com>
2019-05-05 09:31:10 +0800

    550055

--- typeScript/index.js
+++ typeScript/index.js
@@ -34,6 +34,14 @@
     var message = "Hello World";
     console.log(message);
 }
+{
+    var message = "Hello World";
+    console.log(message);
+}
+{
+    var message = "Hello World";
+    console.log(message);
+}
 ;
 {
     var message = "Hello World";
